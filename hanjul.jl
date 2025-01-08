3f99df0ccf5341b30aa4c56791d403dd6ddb498f
Jaewoo Joung <89329303+JaewooJoung@users.noreply.github.com>
2025-01-08 15:20:37 +0100

    Create hanjul.jl

---
+++ hanjul.jl
@@ -0,0 +1,117 @@
+old_korean = Dict(
+    "single_consonants" => Dict(
+        "ᄀ" => Dict("name" => "기역", "modern" => "ㄱ", "phonetic" => "/g/"),
+        "ᄁ" => Dict("name" => "쌍기역", "modern" => "ㄲ", "phonetic" => "/k͈/"),
+        "ᄂ" => Dict("name" => "니은", "modern" => "ㄴ", "phonetic" => "/n/"),
+        "ᄃ" => Dict("name" => "디귿", "modern" => "ㄷ", "phonetic" => "/d/"),
+        "ᄄ" => Dict("name" => "쌍디귿", "modern" => "ㄸ", "phonetic" => "/t͈/"),
+        "ᄅ" => Dict("name" => "리을", "modern" => "ㄹ", "phonetic" => "/r,l/"),
+        "ᄆ" => Dict("name" => "미음", "modern" => "ㅁ", "phonetic" => "/m/"),
+        "ᄇ" => Dict("name" => "비읍", "modern" => "ㅂ", "phonetic" => "/b/"),
+        "ᄈ" => Dict("name" => "쌍비읍", "modern" => "ㅃ", "phonetic" => "/p͈/"),
+        "ᄉ" => Dict("name" => "시옷", "modern" => "ㅅ", "phonetic" => "/s/"),
+        "ᄊ" => Dict("name" => "쌍시옷", "modern" => "ㅆ", "phonetic" => "/s͈/"),
+        "ᄋ" => Dict("name" => "이응", "modern" => "ㅇ", "phonetic" => "null"),
+        "ᄌ" => Dict("name" => "지읒", "modern" => "ㅈ", "phonetic" => "/dʑ/"),
+        "ᄍ" => Dict("name" => "쌍지읒", "modern" => "ㅉ", "phonetic" => "/t͈ɕ/"),
+        "ᄎ" => Dict("name" => "치읓", "modern" => "ㅊ", "phonetic" => "/tɕʰ/"),
+        "ᄏ" => Dict("name" => "키읔", "modern" => "ㅋ", "phonetic" => "/kʰ/"),
+        "ᄐ" => Dict("name" => "티읕", "modern" => "ㅌ", "phonetic" => "/tʰ/"),
+        "ᄑ" => Dict("name" => "피읖", "modern" => "ㅍ", "phonetic" => "/pʰ/"),
+        "ᄒ" => Dict("name" => "히읗", "modern" => "ㅎ", "phonetic" => "/h/")
+    ),
+    
+    "extinct_consonants" => Dict(
+        "ㆁ" => Dict("name" => "옛이응", "phonetic" => "/ŋ/", "description" => "Velar nasal"),
+        "ㆆ" => Dict("name" => "옛시옷", "phonetic" => "/ʔ/", "description" => "Glottal stop"),
+        "ㅿ" => Dict("name" => "지읒", "phonetic" => "/z/", "description" => "Voiced fricative")
+    ),
+    
+    "complex_consonants" => Dict(
+        "ᆪ" => Dict("name" => "옛기역시옷", "phonetic" => "/ks/"),
+        "ᆬ" => Dict("name" => "옛니은지읒", "phonetic" => "/nz/"),
+        "ᆭ" => Dict("name" => "옛니은히읗", "phonetic" => "/nh/"),
+        "ᆰ" => Dict("name" => "옛리을기역", "phonetic" => "/lg/"),
+        "ᆱ" => Dict("name" => "옛리을미음", "phonetic" => "/lm/"),
+        "ᆲ" => Dict("name" => "옛리을비읍", "phonetic" => "/lb/"),
+        "ᆳ" => Dict("name" => "옛리을시옷", "phonetic" => "/ls/"),
+        "ᆴ" => Dict("name" => "옛리을티읕", "phonetic" => "/lth/"),
+        "ᆵ" => Dict("name" => "옛리을피읖", "phonetic" => "/lph/"),
+        "ᆶ" => Dict("name" => "옛리을히읗", "phonetic" => "/lh/"),
+        "ᆷ" => Dict("name" => "옛미음", "phonetic" => "/m/"),
+        "ᆸ" => Dict("name" => "옛비읍", "phonetic" => "/b/"),
+        "ᆹ" => Dict("name" => "옛비읍시옷", "phonetic" => "/ps/"),
+        "ᆺ" => Dict("name" => "옛시옷", "phonetic" => "/s/"),
+        "ᆻ" => Dict("name" => "옛쌍시옷", "phonetic" => "/ss/"),
+        "ᆼ" => Dict("name" => "옛이응", "phonetic" => "/ng/"),
+        "ᆽ" => Dict("name" => "옛지읒", "phonetic" => "/j/"),
+        "ᆾ" => Dict("name" => "옛치읓", "phonetic" => "/ch/"),
+        "ᆿ" => Dict("name" => "옛키읔", "phonetic" => "/kh/"),
+        "ᇀ" => Dict("name" => "옛티읕", "phonetic" => "/th/"),
+        "ᇁ" => Dict("name" => "옛피읖", "phonetic" => "/ph/"),
+        "ᇂ" => Dict("name" => "옛히읗", "phonetic" => "/h/"),
+        "ᄼ" => Dict("name" => "옛이응지읒", "phonetic" => "/nj/"),
+        "ᄾ" => Dict("name" => "옛니은디귿", "phonetic" => "/nt/"),
+        "ᄿ" => Dict("name" => "옛리을기역", "phonetic" => "/lk/"),
+        "ᅀ" => Dict("name" => "옛리을지읒", "phonetic" => "/lj/"),
+        "ᅌ" => Dict("name" => "옛지읒", "phonetic" => "/z/"),
+        "ᅍ" => Dict("name" => "옛치읓", "phonetic" => "/ch/"),
+        "ᅎ" => Dict("name" => "옛키읔", "phonetic" => "/kh/"),
+        "ᅏ" => Dict("name" => "옛티읕", "phonetic" => "/th/"),
+        "ᅐ" => Dict("name" => "옛피읖", "phonetic" => "/ph/"),
+        "ᅑ" => Dict("name" => "옛히읗", "phonetic" => "/h/"),
+        "ᇫ" => Dict("name" => "옛쌍지읒", "phonetic" => "/jj/"),
+        "ᇰ" => Dict("name" => "옛니은지읒", "phonetic" => "/nz/"),
+        "ᇹ" => Dict("name" => "옛리을", "phonetic" => "/l/")
+    ),
+    
+    "vowels" => Dict(
+        "basic" => Dict(
+            "ㅏ" => Dict("name" => "아", "phonetic" => "/a/"),
+            "ㅑ" => Dict("name" => "야", "phonetic" => "/ja/"),
+            "ㅓ" => Dict("name" => "어", "phonetic" => "/ə/"),
+            "ㅕ" => Dict("name" => "여", "phonetic" => "/jə/"),
+            "ㅗ" => Dict("name" => "오", "phonetic" => "/o/"),
+            "ㅛ" => Dict("name" => "요", "phonetic" => "/jo/"),
+            "ㅜ" => Dict("name" => "우", "phonetic" => "/u/"),
+            "ㅠ" => Dict("name" => "유", "phonetic" => "/ju/"),
+            "ㅡ" => Dict("name" => "으", "phonetic" => "/ɯ/"),
+            "ㅣ" => Dict("name" => "이", "phonetic" => "/i/")
+        ),
+        "extinct" => Dict(
+            "ㆍ" => Dict("name" => "아래아", "phonetic" => "between /a/ and /ə/"),
+            "ᆢ" => Dict("name" => "쌍아래아", "description" => "Double dot vowel"),
+            "ㆎ" => Dict("name" => "아래아이", "description" => "Combined form"),
+            "ᆝ" => Dict("name" => "아래아어", "description" => "Combined form"),
+            "ᆞ" => Dict("name" => "아래아으", "description" => "Combined form"),
+            "ᆟ" => Dict("name" => "아래아이", "description" => "Another variant"),
+            "ᆡ" => Dict("name" => "아래아애", "description" => "Combined form"),
+            "ᆣ" => Dict("name" => "아래아요", "description" => "Combined form"),
+            "ᆤ" => Dict("name" => "아래아윤", "description" => "Combined form"),
+            "ᆥ" => Dict("name" => "아래아은", "description" => "Combined form"),
+            "ᆦ" => Dict("name" => "아래아인", "description" => "Combined form"),
+            "ᆧ" => Dict("name" => "아래아올", "description" => "Combined form"),
+            "ᆨ" => Dict("name" => "아래아웃", "description" => "Combined form")
+        ),
+        "compound" => Dict(
+            "ㅐ" => Dict("name" => "애", "phonetic" => "/ɛ/"),
+            "ㅒ" => Dict("name" => "얘", "phonetic" => "/jɛ/"),
+            "ㅔ" => Dict("name" => "에", "phonetic" => "/e/"),
+            "ㅖ" => Dict("name" => "예", "phonetic" => "/je/"),
+            "ㅘ" => Dict("name" => "와", "phonetic" => "/wa/"),
+            "ㅙ" => Dict("name" => "왜", "phonetic" => "/wɛ/"),
+            "ㅚ" => Dict("name" => "외", "phonetic" => "/we/"),
+            "ㅝ" => Dict("name" => "워", "phonetic" => "/wə/"),
+            "ㅞ" => Dict("name" => "웨", "phonetic" => "/we/"),
+            "ㅟ" => Dict("name" => "위", "phonetic" => "/wi/"),
+            "ㅢ" => Dict("name" => "의", "phonetic" => "/ɰi/")
+        )
+    ),
+    
+    "tonal_marks" => Dict(
+        "·" => Dict("name" => "평성", "description" => "Low tone"),
+        ":" => Dict("name" => "상성", "description" => "Rising tone"),
+        ".." => Dict("name" => "거성", "description" => "High tone")
+    )
+)
+
